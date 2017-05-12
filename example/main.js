579b5c46cbec86084c92fcf9a704e44f85e6dcad
Nick Peihl <nickp@sanjuanco.com>
2017-05-12 08:58:14 -0700

    Better example using leaflet and leaflet-draw
    Creates, edits, and deletes are all stored as append logs in the
    browser's IndexedDB database. There is a lot of sugar applied in the
    example's main.js file. It might be worth working this into a separate
    module like osm-p2p-geojson does for osm-p2p-db.

--- example/main.js
+++ example/main.js
@@ -1,43 +1,118 @@
+/* global L */
+
 var hyperlog = require('hyperlog')
 var level = require('level-browserify')
 var idbstore = require('idb-chunk-store')
-var dragDrop = require('drag-drop')
-var geojsonhint = require('@mapbox/geojsonhint')
+var randomBytes = require('randombytes')
+var sampleFeatures = require('./sample.json')
+var turf = require('@turf/helpers')
+var sublevel = require('subleveldown')
+require('leaflet-draw')
 
-var db = {
-  log: level('log'),
-  index: level('index')
-}
+var map = L.map('map', {
+  center: [48.5, -123.0],
+  zoom: 11
+})
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map)
+
+var gjLayer = L.geoJSON()
+    .bindPopup(function (layer) {
+      return layer.feature.id
+    })
+    .addTo(map)
+
+map.addControl(new L.Control.Draw({
+  edit: {
+    featureGroup: gjLayer,
+    poly: {
+      allowIntersection: false
+    }
+  }
+}))
+
+var db = level('gjdb')
 
 var gjdb = require('../')
 window.gj = gjdb({
-  log: hyperlog(db.log, { valueEncoding: 'json' }),
-  db: db.index,
+  log: hyperlog(sublevel(db, 'log'), { valueEncoding: 'json' }),
+  db: sublevel(db, 'index'),
   store: idbstore(4096)
 })
 
-var el = document.createElement('div')
-el.setAttribute('id', 'dropTarget')
-el.setAttribute('style', 'width:100vw; height:100vh;')
-el.innerText = 'Drag and Drop a GeoJSON file here to store in the database'
-document.body.appendChild(el)
+initMapListeners()
 
-dragDrop('#dropTarget', function (files) {
-  files.forEach(function (file) {
-    var reader = new window.FileReader()
-    reader.addEventListener('load', function (e) {
-      var arr = new Uint8Array(e.target.result)
-      var buf = new Buffer(arr)
-      geojsonhint.hint(buf.toString())
-      var data = JSON.parse(buf.toString())
-      if (data.features && Array.isArray(data.features)) {
-        data.features.map(function (f) {
-          window.gj.create(f)
+window.gj.ready(function () {
+  var features = sampleFeatures.features;
+  (function next () {
+    if (features.length === 0) return ready()
+    var feat = features.shift()
+    var key = feat.id ? feat.id : randomBytes(8).toString('hex')
+    feat.id = key
+    window.gj.get(key, function (err, node) {
+      if (err) throw err
+      if (Object.keys(node).length === 0) {
+        console.log('feature does not exist yet')
+        window.gj.put(key, feat, function (err) {
+          if (err) throw err
+          next()
         })
       } else {
-        window.gj.create(data)
+        console.log('feature exists')
+        next()
       }
     })
-    reader.readAsArrayBuffer(file)
-  })
+  })()
 })
+
+function ready () {
+  window.gj.log.heads(function (err, heads) {
+    if (err) throw err
+    var fc = turf.featureCollection(heads.filter(function (head) {
+      // Don't return deleted features or null values
+      return !head.value.d && head.value.v
+    }).map(function (head) {
+      // Return geojson value of head
+      return head.value.v
+    }))
+    gjLayer.clearLayers()
+    gjLayer.addData(fc)
+    map.flyToBounds(gjLayer.getBounds())
+  })
+}
+
+function initMapListeners () {
+  map.on('draw:created', function (e) {
+    var feat = e.layer.toGeoJSON()
+    var key = randomBytes(8).toString('hex')
+    feat.id = key
+    window.gj.put(key, feat, function (err, node) {
+      if (err) throw err
+      ready()
+    })
+  })
+
+  map.on('draw:edited', function (e) {
+    var fc = e.layers.toGeoJSON()
+    fc.features.forEach(function (feat) {
+      var key = feat.id ? feat.id : randomBytes(8).toString('hex')
+      feat.id = key
+      window.gj.put(key, feat, function (err, node) {
+        if (err) throw err
+        ready()
+      })
+    })
+  })
+
+  map.on('draw:deleted', function (e) {
+    var fc = e.layers.toGeoJSON()
+    fc.features.forEach(function (feat) {
+      var key = feat.id
+      window.gj.del(key, function (err, node) {
+        if (err) throw err
+        ready()
+      })
+    })
+  })
+}
